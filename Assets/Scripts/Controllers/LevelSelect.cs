926a858c93eae8743478be58d950ca0e11159b80
Aleksy Mieki <alemie183_dpp_2023@e-science.pl>
2023-05-10 23:21:14 +0200

    changed the way the levels are selected, and now all i need is 1 scene for each location and just passing in the initial parameters on level select

--- Assets/Scripts/Controllers/LevelSelect.cs
+++ Assets/Scripts/Controllers/LevelSelect.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelSelect : MonoBehaviour
 {
+    
     public void Level1()
     {   
         SceneManager.LoadScene("LEVEL1");
@@ -15,7 +16,7 @@ public class LevelSelect : MonoBehaviour
     }
     public void Level2()
     {
-        SceneManager.LoadScene("LEVEL2");
+        SceneManager.LoadScene("LEVEL1");
     }
 
     public void SetLevel2Speed()
@@ -26,7 +27,7 @@ public class LevelSelect : MonoBehaviour
 
     public void Level3()
     {
-        SceneManager.LoadScene("LEVEL3");
+        SceneManager.LoadScene("LEVEL1");
     }
     public void SetLevel3Speed()
     {
@@ -35,7 +36,7 @@ public class LevelSelect : MonoBehaviour
     }
     public void Level4()
     {
-        SceneManager.LoadScene("LEVEL4");
+        SceneManager.LoadScene("LEVEL1");
     }
 
     public void SetLevel4Speed()
@@ -46,7 +47,7 @@ public class LevelSelect : MonoBehaviour
 
     public void Level5()
     {
-        SceneManager.LoadScene("LEVEL5");
+        SceneManager.LoadScene("LEVEL1");
     }
 
     public void SetLevel5Speed()
@@ -58,7 +59,7 @@ public class LevelSelect : MonoBehaviour
 
     public void Level6()
     {
-        SceneManager.LoadScene("LEVEL6");
+        SceneManager.LoadScene("LEVEL1");
     }
 
     public void SetLevel6Speed()
@@ -70,12 +71,153 @@ public class LevelSelect : MonoBehaviour
 
     public void Boss1Level()
     {
-        
+        SceneManager.LoadScene("TYPE1BOSS");
     }
 
     public void setBoss1Level()
     {
-        
+        GameManager.instance.setEnemyHealth(50);
+        GameManager.instance.gameSpeed = 9f;
     }
+    
+    public void Level2_1()
+    {   
+        SceneManager.LoadScene("LEVEL2.1");
+    }
+    public void SetLevel2_1Speed()
+    {
+        GameManager.instance.setEnemyHealth(10);
+        GameManager.instance.gameSpeed = 4f;
+    }
+    public void Level2_2()
+    {
+        SceneManager.LoadScene("LEVEL2.1");
+    }
+
+    public void SetLevel2_2Speed()
+    {
+        GameManager.instance.setEnemyHealth(20);
+        GameManager.instance.gameSpeed = 5f;
+    }
+
+    public void Level2_3()
+    {
+        SceneManager.LoadScene("LEVEL2.1");
+    }
+    public void SetLevel2_3Speed()
+    {
+        GameManager.instance.setEnemyHealth(20);
+        GameManager.instance.gameSpeed = 6f;
+    }
+    public void Level2_4()
+    {
+        SceneManager.LoadScene("LEVEL2.1");
+    }
+
+    public void SetLevel2_4Speed()
+    {
+        GameManager.instance.setEnemyHealth(30);
+        GameManager.instance.gameSpeed = 8f;
+    }
+
+    public void Level2_5()
+    {
+        SceneManager.LoadScene("LEVEL2.1");
+    }
+
+    public void SetLevel2_5Speed()
+    {
+        GameManager.instance.setEnemyHealth(50);
+        GameManager.instance.gameSpeed = 9f;
+
+    }
+
+    public void Level2_6()
+    {
+        SceneManager.LoadScene("LEVEL2.1");
+    }
+
+    public void SetLevel2_6Speed()
+    {
+        GameManager.instance.setEnemyHealth(50);
+        GameManager.instance.gameSpeed = 9f;
+
+    }
+
+    public void Boss2Level()
+    {
+        SceneManager.LoadScene("TYPE2BOSS");
+    }
+
+    public void setBoss2Level()
+    {
+        GameManager.instance.setEnemyHealth(50);
+        GameManager.instance.gameSpeed = 9f;
+    }
+      
+    public void Level3_1()
+    {   
+        SceneManager.LoadScene("LEVEL1.3");
+    }
+    public void SetLevel3_1Speed()
+    {
+        GameManager.instance.setEnemyHealth(10);
+        GameManager.instance.gameSpeed = 4f;
+    }
+    public void Level3_2()
+    {
+        SceneManager.LoadScene("LEVEL1.3");
+    }
+
+    public void SetLevel3_2Speed()
+    {
+        GameManager.instance.setEnemyHealth(20);
+        GameManager.instance.gameSpeed = 5f;
+    }
+
+    public void Level3_3()
+    {
+        SceneManager.LoadScene("LEVEL1.3");
+    }
+    public void SetLevel3_3Speed()
+    {
+        GameManager.instance.setEnemyHealth(20);
+        GameManager.instance.gameSpeed = 6f;
+    }
+    public void Level3_4()
+    {
+        SceneManager.LoadScene("LEVEL1.3");
+    }
+
+    public void SetLevel3_4Speed()
+    {
+        GameManager.instance.setEnemyHealth(30);
+        GameManager.instance.gameSpeed = 8f;
+    }
+
+    public void Level3_5()
+    {
+        SceneManager.LoadScene("LEVEL1.3");
+    }
+
+    public void SetLevel3_5Speed()
+    {
+        GameManager.instance.setEnemyHealth(50);
+        GameManager.instance.gameSpeed = 9f;
+
+    }
+
+    public void Level3_6()
+    {
+        SceneManager.LoadScene("LEVEL1.3");
+    }
+
+    public void SetLevel3_6Speed()
+    {
+        GameManager.instance.setEnemyHealth(50);
+        GameManager.instance.gameSpeed = 9f;
+
+    }
+
 
 }
--- Assets/Scripts/Controllers/MyTimer.cs
+++ Assets/Scripts/Controllers/MyTimer.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 
 public class MyTimer : MonoBehaviour
 {
-   
+    private static int k = 0;
     static public IEnumerator CountUpTo1Minute()
     {
-        yield return new WaitForSeconds(60);
+        yield return new WaitForSeconds(6);
        
         OnTimerElapsed();
     }
    
     private static void OnTimerElapsed()
     {
-        if (GameManager.instance.getCurrentScene() == "LEVEL1") LevelCompletionManager.instance.levelCompletionStatus[0] = true;
-        if (GameManager.instance.getCurrentScene() == "LEVEL2") LevelCompletionManager.instance.levelCompletionStatus[1] = true;
-        if (GameManager.instance.getCurrentScene() == "LEVEL3") LevelCompletionManager.instance.levelCompletionStatus[2] = true;
-        if (GameManager.instance.getCurrentScene() == "LEVEL4") LevelCompletionManager.instance.levelCompletionStatus[3] = true;
+        if (GameManager.instance.getCurrentScene() == "LEVEL1")
+        {
+            LevelCompletionManager.instance.levelCompletionStatus[k] = true;
+            k++;
+        }
     }
+    
 }
