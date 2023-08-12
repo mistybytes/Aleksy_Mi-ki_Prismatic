56adb1db5df7d93ece1d2702901173fab6feef8a
Aleksy Mieki <alemie183_dpp_2023@e-science.pl>
2023-08-12 19:16:52 +0200

    message

--- Assets/Scripts/Enemy_Scripts/BossScripts/BossManager.cs
+++ Assets/Scripts/Enemy_Scripts/BossScripts/BossManager.cs
@@ -1,21 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class BossManager : MonoBehaviour
 {
     private int bossTimesHit = 0;
     private Vector3 position;
+    
+    public void OnDestroy()
+    {
+        //TODO make a lot of coins be spawned
+        StartCoroutine(LoadSceneAfterDelay());
+    }
+
     public void BossHit()
     {
-        bossTimesHit++;
-        if (bossTimesHit == 7)
-        { 
-            Destroy(gameObject);
-            
+        switch (bossTimesHit++)
+        {
+            case 7:
+                Destroy(gameObject);
+                break;
         }
+       
     }
     
+    IEnumerator LoadSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("BossDefeated");
+    }
  
 }
